ba183b1ab9a1cd42a0ce560a07ba80e2c7794851
hasunghwa <tjdghk3920@naver.com>
2022-05-30 17:13:45 +0900

    백엔드 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 
 // 라우팅
@@ -12,6 +13,10 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+// URL을 통해 전달되는 값이 한글, 공백같은 문자 처리 안되는거 해결
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/", home); // use -> 미들웨어
 
 module.exports = app;
