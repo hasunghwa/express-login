da9b09f6ffb84f1ad8aec67de48798858826c1c8
hasunghwa <tjdghk3920@naver.com>
2022-05-31 11:54:27 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,7 +3,7 @@
 const fs = require("fs").promises;
 class UserStorage {
   static #getUserInfo(data, id) {
-    const users = log(JSON.parse(data));
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const userKeys = Object.keys(users);
     const userInfo = userKeys.reduce((newUser, info) => {
@@ -13,10 +13,10 @@ class UserStorage {
 
     return userInfo;
   }
-  
-  static getUsers(...fields) {
-    // 인자로 넘겨받은 필드만
-    //const users = this.#users;
+
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -26,21 +26,33 @@ class UserStorage {
     return newUsers;
   }
 
-  static getUserInfo(id) {
-    // 인자로 받은 id값에 대한 유저정보
+  static getUsers(isAll, ...fields) { // 인자로 넘겨받은 필드만
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch(console.error);
+  }
+
+  static getUserInfo(id) { // 인자로 받은 id값에 대한 유저정보
     return fs
       .readFile("./src/databases/users.json")
       .then((data) => {
         return this.#getUserInfo(data, id);
       })
-      .catch(console.err);
+      .catch(console.error);
   }
 
-  static save(userInfo) {
-    //const users = this.#users;
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다.";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
     return { success: true };
   }
 }
